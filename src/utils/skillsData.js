614f7aa04392b8fc55d2cf657ff0ac677d173512
andresisfx <124216395+andresisfx@users.noreply.github.com>
2024-02-29 22:10:48 -0500

    link fixer updated

--- src/utils/skillsData.js
+++ src/utils/skillsData.js
@@ -96,7 +96,7 @@ export const projects = [
     description:
       "Single page application, completly functional. with login, database, register of user, dashboards, statistics and much more",
     tech: "React, Redux, Javascript, Tailwind, Css, PostgreSQL, Express, Sequelize, Apis, Node.js",
-    link: "https://dev.fixershoes.com/",
+    link: "https://e-commerce.fixershoes.com/",
   },
   {
     name: "Tennis App",
